7ab3c38a74bd52ee216abc81dddcb199d9f4b0c5
Sasha Favorov <favorov@sensi.org>
2025-06-24 21:44:02 -0400

    tests for 4 points work

--- cellboro/tests/testthat/test-delaunay.neighbours-xy.r
+++ cellboro/tests/testthat/test-delaunay.neighbours-xy.r
@@ -1,23 +1,25 @@
 test_that(
-    "delaunay.neighbours for x-y input 3 points", {
-        x <- c(1, 3, 2)
-        y <- c(1, 1, 2)
+    "delaunay.neighbours for x-y input 4 points", {
+        x <- c(1, 3, 3, 5)
+        y <- c(2, 1, 3, 2)
         res_xy <- delaunay.neighbours(x, y)
-        exp_res_xy <- data.frame(
-            ind1 = c(1, 1, 2, 2, 3, 3),
-            ind2 = c(2, 3, 1, 3, 1, 2)
+        exp_res <- data.frame(
+            ind1 = c(1, 1, 2, 2, 2, 3, 3, 3, 4, 4),
+            ind2 = c(2, 3, 1, 3, 4, 1, 2, 4, 2, 3)
         )
-        expect_true(all.equal(res_xy, exp_res_xy))
+        expect_true(all.equal(res_xy, exp_res))
     }
 )
 test_that(
-    "delaunay.neighbours for data.frame x-y input 3 points", {
-        df <- data.frame(x = c(1, 3, 2), y = c(1, 1, 2))
+    "delaunay.neighbours for data.frame x-y input 4 points", {
+        x <- c(1, 3, 3, 5)
+        y <- c(2, 1, 3, 2)
+        df <- data.frame(x, y)
         res_df <- delaunay.neighbours(df)
-        exp_res_df <- data.frame(
-            ind1 = c(1, 1, 2, 2, 3, 3),
-            ind2 = c(2, 3, 1, 3, 1, 2)
+        exp_res <- data.frame(
+            ind1 = c(1, 1, 2, 2, 2, 3, 3, 3, 4, 4),
+            ind2 = c(2, 3, 1, 3, 4, 1, 2, 4, 2, 3)
         )
-        expect_true(all.equal(res_df, exp_res_df))
+        expect_true(all.equal(res_df, exp_res))
     }
 )
